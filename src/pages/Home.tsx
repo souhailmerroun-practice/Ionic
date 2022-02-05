04a780f3074ee947a6af5ffa04d93ca582a9f9c4
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-02-05 10:29:05 +0100

    Testing the LocalNotifications pluging

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { LocalNotifications } from '@awesome-cordova-plugins/local-notifications';
 
 const Home: React.FC = () => {
+
+  const scheduleNotification = () => {
+    LocalNotifications.schedule({
+      id: 1,
+      text: 'Single ILocalNotification',
+      //sound: isAndroid? 'file://sound.mp3': 'file://beep.caf',
+      //data: { secret: key }
+    })
+    console.log("we have it")
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +29,7 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+        <IonButton onClick={scheduleNotification}>Schedule</IonButton>
       </IonContent>
     </IonPage>
   );
